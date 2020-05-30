edc13249fdc4ddb72badf1e95972010b880ad952
Luís Alves <luismig.alves@gmail.com>
2020-05-30 19:22:12 +0100

    Messing around with linters

--- crosswalks-current-status-service/models/CrosswalkStatus.js
+++ crosswalks-current-status-service/models/CrosswalkStatus.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+
+const { Schema } = mongoose;
 
 /**
  * Crosswalk Status Schema
@@ -9,27 +10,30 @@ const crosswalkStatusSchema = new Schema(
     uid: {
       type: String,
       unique: true,
-      required: true
+      required: true,
     },
     light: {
       type: String,
       enum: ['green', 'yellow', 'red'],
-      default: 'green'
+      default: 'green',
     },
     num_vehicles: {
       type: Number,
       min: 0,
-      default: 0
+      default: 0,
     },
     num_pedestrians: {
       type: Number,
       min: 0,
-      default: 0
-    }
+      default: 0,
+    },
   },
-  { timestamps : true }
+  { timestamps: true },
 );
 
-const CrosswalkStatus = mongoose.model('CrosswalkStatus', crosswalkStatusSchema);
+const CrosswalkStatus = mongoose.model(
+  'CrosswalkStatus',
+  crosswalkStatusSchema,
+);
 
 module.exports = CrosswalkStatus;
--- crosswalks-current-status-service/server.js
+++ crosswalks-current-status-service/server.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const CrosswalkStatus = require('./models/CrosswalkStatus');
 const amqp = require('amqplib/callback_api');
+const CrosswalkStatus = require('./models/CrosswalkStatus');
 
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 };
 
 const host = process.env.DB_HOST;
@@ -17,71 +17,69 @@ const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
 const uri = `mongodb://${username}:${password}@${host}:${port}/${database}`;
-mongoose.connect(uri, options).
-  catch(error => {
-    console.log(error);
-  });
-
-const rabbit = `${process.env.RABBIT_HOSTNAME}`
-amqp.connect(`amqp://${rabbit}`, (err, con) => {
-  if (err) {
-    console.log(err);
-    throw err;
-  }
-
-  consume_client(con, 'pedestrian', 'near');
-  consume_client(con, 'pedestrian', 'far');
-  consume_client(con, 'vehicle', 'near');
-  consume_client(con, 'vehicle', 'far');
+mongoose.connect(uri, options).catch((error) => {
+  console.log(error);
 });
 
-// retrieves the current status of the crosswalk
-async function get_crosswalk_current_status(crosswalk_id) {
+/**
+ * Retrieves the current status of the crosswalk
+ * @param {string} crosswalkId
+ */
+async function getCrosswalkCurrentStatus(crosswalkId) {
   let crosswalkStatus;
-  const filter = { uid: crosswalk_id };
+  const filter = { uid: crosswalkId };
   try {
-    crosswalkStatus = await CrosswalkStatus.findOneAndUpdate(filter, filter, { upsert: true, new: true });
-  }
-  catch (err) {
-    console.log('Could not get current Crosswalk Status ' + err);
+    crosswalkStatus = await CrosswalkStatus.findOneAndUpdate(filter, filter, {
+      upsert: true,
+      new: true,
+    });
+  } catch (err) {
+    console.log(`Could not get current Crosswalk Status ${err}`);
   }
 
   const status = {
     light: crosswalkStatus.light,
     num_peds: crosswalkStatus.num_pedestrians,
-    num_vehs: crosswalkStatus.num_vehicles
+    num_vehs: crosswalkStatus.num_vehicles,
   };
   return JSON.stringify(status);
 }
 
-// Given a crosswalk_id, an inc_value and a client_field, updates the crosswalk status
-// if inc_value = -1, it decreases the client_field. If 1, it increases
-// Valid client_fields: num_pedestrians; num_vehicles
-async function update_count_in_crosswalk(crosswalk_id, inc_value, client_field) {
-  let increment = {};
-  increment[client_field] = inc_value;
+/**
+ * Update the crosswalk status given those params
+ * Valid inc_values: -1, 1
+ * Valid client_fields: num_pedestrians; num_vehicles
+ * @param {string} crosswalkId
+ * @param {number} incValue
+ * @param {string} clientField
+ */
+async function updateCountInCrosswalk(crosswalkId, incValue, clientField) {
+  const increment = {};
+  increment[clientField] = incValue;
   try {
     await CrosswalkStatus.findOneAndUpdate(
-        { uid: crosswalk_id },
-        { $inc: increment}
-      );
-  }
-  catch (err) {
-    if (inc_value < 0) {
-      console.log(`Could not decrease ${client_field} from ${crosswalk_id}`);
-    }
-    else {
-      console.log(`Could not increase ${client_field} to ${crosswalk_id}`);
+      { uid: crosswalkId },
+      { $inc: increment },
+    );
+  } catch (err) {
+    if (incValue < 0) {
+      console.log(`Could not decrease ${clientField} from ${crosswalkId}`);
+    } else {
+      console.log(`Could not increase ${clientField} to ${crosswalkId}`);
     }
     console.log(err);
   }
 }
 
-// Given a crosswalk_id, send a message to subscribers with current crosswalk status
-function produce_crosswalk_status_short(con, crosswalk_id) {
+/**
+ * Given an id, produce a status message to subscribers
+ * @param {amqp.connection} con
+ * @param {string} crosswalkId
+ */
+function produceCrosswalkStatusShort(con, crosswalkId) {
   con.createChannel((err, ch) => {
     const exchange = 'public';
-    const key = `${crosswalk_id}.status.short`;
+    const key = `${crosswalkId}.status.short`;
 
     if (err) {
       console.log(`Could not create Channel to produce to ${key}`);
@@ -91,19 +89,23 @@ function produce_crosswalk_status_short(con, crosswalk_id) {
 
     ch.assertExchange(exchange, 'topic', { durable: true });
 
-    get_crosswalk_current_status(crosswalk_id)
-      .then(status_msg => {
-          console.log(`Published ${status_msg}`);
-          ch.publish(exchange, key, Buffer.from(status_msg));
-        }
-      );
+    getCrosswalkCurrentStatus(crosswalkId).then((statusMsg) => {
+      console.log(`Published ${statusMsg}`);
+      ch.publish(exchange, key, Buffer.from(statusMsg));
+    });
   });
 }
 
-// Given an action, activates a consumer with the key *.<client>.<action>
-// clients: vehicle or pedestrian
-// actions: near or far
-function consume_client(con, client, action) {
+/**
+ * Given these params, activates a consumer with
+ * key *.<client>.<action>
+ * Valid clients: vehicle; pedestrian
+ * Valid actions: near; far
+ * @param {amqp.connection} con
+ * @param {string} client
+ * @param {string} action
+ */
+function consumeClient(con, client, action) {
   con.createChannel((err, ch) => {
     if (err) {
       console.log(err);
@@ -112,28 +114,46 @@ function consume_client(con, client, action) {
 
     const exchange = 'private';
     const key = `*.${client}.${action}`;
-    const inc_value = (action == 'near') ? 1 : -1;
-    const client_field = `num_${client}s`;
+    const incValue = action === 'near' ? 1 : -1;
+    const clientField = `num_${client}s`;
 
     ch.assertExchange(exchange, 'topic', { durable: true });
 
-    ch.assertQueue('', { exclusive: true, autoDelete: true, durable: true }, (err, q) => {
-      if (err) {
-        console.log(`Could not assert queue to consume ${key}`);
-      }
+    ch.assertQueue(
+      '',
+      { exclusive: true, autoDelete: true, durable: true },
+      (err2, q) => {
+        if (err2) {
+          console.log(`Could not assert queue to consume ${key}`);
+        }
 
-      ch.bindQueue(q.queue, exchange, key);
-      
-      console.log(`Now consuming key (${key})`);
-      ch.consume(q.queue, (msg) => {
-        const who = JSON.parse(msg.content.toString());
-        update_count_in_crosswalk(who.crosswalk_id, inc_value, client_field)
-          .then( () => {
-            console.log(`consumed: ${JSON.stringify(who)}`);
-            ch.ack(msg);
-            produce_crosswalk_status_short(con, who.crosswalk_id);
-          });
-      });
-    });
+        ch.bindQueue(q.queue, exchange, key);
+
+        console.log(`Now consuming key (${key})`);
+        ch.consume(q.queue, (msg) => {
+          const who = JSON.parse(msg.content.toString());
+          updateCountInCrosswalk(who.crosswalk_id, incValue, clientField).then(
+            () => {
+              console.log(`consumed: ${JSON.stringify(who)}`);
+              ch.ack(msg);
+              produceCrosswalkStatusShort(con, who.crosswalk_id);
+            },
+          );
+        });
+      },
+    );
   });
 }
+
+const rabbit = `${process.env.RABBIT_HOSTNAME}`;
+amqp.connect(`amqp://${rabbit}`, (err, con) => {
+  if (err) {
+    console.log(err);
+    throw err;
+  }
+
+  consumeClient(con, 'pedestrian', 'near');
+  consumeClient(con, 'pedestrian', 'far');
+  consumeClient(con, 'vehicle', 'near');
+  consumeClient(con, 'vehicle', 'far');
+});
