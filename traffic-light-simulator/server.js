77b5859fd525933f61409e9741da2cf6ff65ef54
rafaelacrr <rafaelaricorodrigues@gmail.com>
2020-06-01 18:57:03 +0100

    Fixed some flaws and lapses

--- traffic-light-simulator/server.js
+++ traffic-light-simulator/server.js
@@ -1,16 +1,18 @@
 const fetch = require("node-fetch");
 const amqp = require('amqplib/callback_api');
 
-var crosswalks = [];
-var channel;
-var exchange = 'private'
-var iter = 0;
+const crosswalks = [];
+const channel = null;
+const exchange = 'private'
+const iter = 0;
 const colors = ["green","yellow","red"];
+const crosswalk_location = process.env.CROSSWALKS_LOCATION_SERVICE_HOSTNAME;
+const rabbit = process.env.RABBIT_HOSTNAME;
 
 
 //Get all crosswalk in the system
 async function getCrosswalks () {
-  await fetch('http://crosswalks_location_service:3000/v1/crosswalks/')
+  await fetch('http://${crosswalks_location}/v1/crosswalks/')
     .then( async (response) => {
         const resp = await response.json();
         crosswalks=crosswalks.concat(resp.crosswalks);
@@ -21,7 +23,7 @@ async function getCrosswalks () {
 };
 
 
-// For each crosswalk, adds the light color and pushishes to the queue
+// For each crosswalk, adds the light color and publishes to the queue
 function sendInformation () {
   //Give signal color
   crosswalks.forEach(crosswalk => {
@@ -32,17 +34,18 @@ function sendInformation () {
     //send information to the queue
     const key = `${crosswalk.id}.light.update`;
     console.log(key);
-    var msg = JSON.stringify(crosswalkTL);
+    const msg = JSON.stringify(crosswalkTL);
     channel.publish(exchange, key, Buffer.from(msg));
     console.log(" [x] Sent %s", msg);
   });
   iter=(++iter)%3;
+  setTimeout(sendInformation,5000)
 };
 
 
 //Starts connection with rabbitmq
 function startConnection(){
-  amqp.connect('amqp://rabbit', (connError, connection) => {
+  amqp.connect('amqp://${rabbit}', (connError, connection) => {
     if(connError){
       throw connError;
     }
@@ -51,7 +54,7 @@ function startConnection(){
       if(channelError){
         throw channelError;
       }
-      ch.assertExchange(exchange, 'direct', {
+      ch.assertExchange(exchange, 'topic', {
         durable: true
       });
       channel = ch;
@@ -61,12 +64,13 @@ function startConnection(){
 
 
 function simulate(){
+  getCrosswalks();
   startConnection();
-  setInterval(sendInformation,5000);
+  setTimeout(sendInformation,5000);
 }
 
 
   console.log(`App started`);
+  
   //waiting for the other services to start
-  setTimeout(getCrosswalks,5000);
-  setTimeout(simulate,30000);
+  setTimeout(simulate,5000);
