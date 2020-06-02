880731d3d2f7572770f0c2ed1d9f0e3b28d90adb
rafaelacrr <36546893+rafaelacrr@users.noreply.github.com>
2020-06-02 10:59:54 +0100

    Traffic light simulator [Second version] (#24)
    * traffic light simulator
    
    * Removed test
    
    * Added routing key + fixed details
    
    * Added routing key
    
    * Fixed some flaws and lapses
    
    * Server restarts until rabbit is ready

--- traffic-light-simulator/server.js
+++ traffic-light-simulator/server.js
@@ -1,16 +1,77 @@
-'use strict';
+const fetch = require("node-fetch");
+const amqp = require('amqplib/callback_api');
 
-const express = require('express');
+var crosswalks = [];
+var channel = null;
+const exchange = 'private'
+var iter = 0;
+const colors = ["green","yellow","red"];
+const crosswalk_location = process.env.CROSSWALKS_LOCATION_SERVICE_HOSTNAME;
+const rabbit = process.env.RABBIT_HOSTNAME;
 
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
 
-const server = app.listen(8080, () => {
-  const host = server.address().address;
-  const port = server.address().port;
+/**
+ * Gets all the crosswalks from crosswalks_location_service
+ */
+async function getCrosswalks () {
+  const url = `http://${crosswalk_location}/v1/crosswalks/`;
+  await fetch(url)
+    .then( async (response) => {
+        const resp = await response.json();
+        crosswalks=crosswalks.concat(resp.crosswalks);
+    })
+    .catch((reason) => {
+       console.log(reason);
+    });
+};
 
-  console.log(`App listening at http://${host}:${port}`);
-});
 
+/**
+ * For each crosswalk, adds the light color and publishes it to the queue
+ */
+function sendInformation () {
+  //Give signal color
+  crosswalks.forEach(crosswalk => {
+    const crosswalkTL = {
+        crosswalk_id: crosswalk.id,
+        light: colors[iter%3],
+    }
+    //send information to the queue
+    const key = `${crosswalk.id}.light.update`;
+    const msg = JSON.stringify(crosswalkTL);
+    channel.publish(exchange, key, Buffer.from(msg));
+    console.log(" [x] Sent %s", msg);
+  });
+  iter=(++iter)%3;
+  setTimeout(sendInformation,5000)
+};
+
+
+
+/**
+ * Starts connection with rabbitmq
+ */
+function startConnection(){
+  const url=`amqp://${rabbit}`
+  amqp.connect(url, (connError, connection) => {
+    if(connError){
+      throw connError;
+    }
+    //Create Channel
+    connection.createChannel((channelError, ch) => {
+      if(channelError){
+        throw channelError;
+      }
+      ch.assertExchange(exchange, 'topic', {
+        durable: true
+      });
+      channel = ch;
+    });
+  });
+};
+
+
+  console.log(`App started`);
+  getCrosswalks();
+  startConnection();
+  setTimeout(sendInformation,5000);
