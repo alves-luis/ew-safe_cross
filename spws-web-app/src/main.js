64912bc4f24719ae39ed36d2988ba2bad935ebdf
bishop19 <pedrombm19@gmail.com>
2020-06-03 00:01:28 +0100

    fix stomp

--- spws-web-app/src/main.js
+++ spws-web-app/src/main.js
@@ -18,12 +18,6 @@ import createMockServer from "./mock/server";
 createMockServer();
 
 
-// Stomp
-import VueStomp from "vue-stomp"
-
-Vue.use(VueStomp, "http://localhost:15674/ws");
-
-
 new Vue({
   el: '#app',
   render: h => h(App)
