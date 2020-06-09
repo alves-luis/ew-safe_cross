2a40892c05643b6dbd69cc638a3ea94528633384
rafaelacrr <rafaelaricorodrigues@gmail.com>
2020-06-09 23:08:10 +0100

    Some improvements

--- crosswalks-logger-service/server.js
+++ crosswalks-logger-service/server.js
@@ -29,27 +29,25 @@ mongoose.connect(uri, options).catch((error) => {
   console.log(error);
 });
 
-const exchange = 'private';
-let channel;
-
 /**
  * Starts the connection with rabbit
  */
 function startConnection(){
 
+  const exchange = 'private';
   const url=`amqp://${rabbit}`
+
   amqp.connect(url, function(error0, connection) {
     if (error0) {
       throw error0;
     }
-    connection.createChannel(function(error1, ch) {
+    connection.createChannel(function(error1, channel) {
       if (error1) {
         throw error1;
       }
-      ch.assertExchange(exchange, 'topic', {durable: true});
-      channel = ch;
-      consumeInformation('pedestrian');
-      consumeInformation('vehicle');
+      channel.assertExchange(exchange, 'topic', {durable: true});
+      consumeInformation(channel, exchange, 'pedestrian');
+      consumeInformation(channel, exchange, 'vehicle');
     });
   });
 };
@@ -57,7 +55,7 @@ function startConnection(){
 /**
  * Consumes information with key="*.client.near"
  */
-function consumeInformation(client) {
+function consumeInformation(channel, exchange, client) {
 
   const key = `*.${client}.near`;
 
@@ -135,59 +133,9 @@ async function getInfo(id){
     console.log(error);
   }
   const result = {num_ped: numPed, num_veh: numVeh}
+  console.log(result);
   return JSON.stringify(result);
 }
-
-
-/**
- * Simulator
- */
-/*
-function simulator(){
-
-  const info1={veh_id:11, crosswalk_id:'eeebf21e-ceec-423d-899d-fe92564b7f79' };
-  const info3={veh_id:11, crosswalk_id:'eeebf21e-ceec-423d-899d-fe92564b7f79' };
-  const info2={ped_id:44, crosswalk_id:'68346ef5-2ab8-48ac-a90a-c69b13d627e3' };
-  const info4={ped_id:45, crosswalk_id:'68346ef5-2ab8-48ac-a90a-c69b13d627e3' };
-  const info5={veh_id:6, crosswalk_id:'68346ef5-2ab8-48ac-a90a-c69b13d627e3' };
-
-
-  const exchange = 'private';
-  amqp.connect('amqp://rabbit', (connError, connection) => {
-    if(connError){
-      throw connError;
-    }
-    //Create Channel
-    connection.createChannel((channelError, ch) => {
-      if(channelError){
-        throw channelError;
-      }
-      ch.assertExchange(exchange, 'topic', {
-        durable: true
-      });
-      const key1 = `1.pedestrian.near`;
-      const msg1 = JSON.stringify(info1);
-      const msg2 = JSON.stringify(info2);
-      const msg3 = JSON.stringify(info3);
-      const msg4 = JSON.stringify(info4);
-      const msg5 = JSON.stringify(info5);
-
-      ch.publish(exchange, key1, Buffer.from(msg1));
-      ch.publish(exchange, key1, Buffer.from(msg2));
-      ch.publish(exchange, key1, Buffer.from(msg3));
-      ch.publish(exchange, key1, Buffer.from(msg4));
-      ch.publish(exchange, key1, Buffer.from(msg5));
-      console.log(" MANDEIII Sent %s", msg1);
-    });
-  });
-};
-
-
-function seeInfo(){
-  console.log(getInfo('eeebf21e-ceec-423d-899d-fe92564b7f79'));
-  console.log(getInfo('68346ef5-2ab8-48ac-a90a-c69b13d627e3'));
-}
-*/
 
 
 app.get('/v1/crosswalks/:id', (req, res) => {
@@ -219,6 +167,8 @@ const server = app.listen(3000, () => {
 
   console.log(`App listening at http://${host}:${port}`);
   startConnection();
+  setTimeout(simulator,5000);
+  setTimeout(seeInfo,8000);
 });
 
 module.exports = () => start();
