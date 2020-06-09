9e313983e308b66881e717ec2282cd593c6041c3
rafaelacrr <rafaelaricorodrigues@gmail.com>
2020-06-09 22:42:23 +0100

    Final version

--- crosswalks-logger-service/server.js
+++ crosswalks-logger-service/server.js
@@ -5,7 +5,6 @@ const mongoose = require('mongoose');
 const amqp = require('amqplib/callback_api');
 const CrosswalksHistory = require('./models/CrosswalksHistory');
 
-
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,48 +21,62 @@ const port = process.env.DB_PORT;
 const database = process.env.DB_DATABASE;
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
+const rabbit = process.env.RABBIT_HOSTNAME;
 
-var channel = null;
-const exchange = 'private';
 
 const uri = `mongodb://${username}:${password}@${host}:${port}/${database}`;
 mongoose.connect(uri, options).catch((error) => {
   console.log(error);
 });
 
-/**
- * Consumes information with key="*.client.near"
- */
-function consumeInformation (client) {
+const exchange = 'private';
+let channel;
 
-  const key = `*.${client}.near`;
-  amqp.connect('amqp://rabbit', function(error0, connection) {
+/**
+ * Starts the connection with rabbit
+ */
+function startConnection(){
+
+  const url=`amqp://${rabbit}`
+  amqp.connect(url, function(error0, connection) {
     if (error0) {
       throw error0;
     }
-    connection.createChannel(function(error1, channel) {
+    connection.createChannel(function(error1, ch) {
       if (error1) {
         throw error1;
       }
-      channel.assertExchange(exchange, 'topic', {durable: true});
-      channel.assertQueue(
-        '',
-        {exclusive: true, autoDelete: true, durable: true },
-        (error2, q) => {
-          if (error2) {
-            console.log(`Could not assert queue to consume ${key}`);
-          }
-          channel.bindQueue(q.queue, exchange, key);
-          console.log(` Now consuming key (${key})`);
-          //consumes message
-          channel.consume(q.queue, function(msg) {
-             const message = JSON.parse(msg.content.toString());
-             addCrosswalkHistory(message);
-             console.log(" Message received '%s'", message);
-        }, {
-          noAck: false
-        });
-      });
+      ch.assertExchange(exchange, 'topic', {durable: true});
+      channel = ch;
+      consumeInformation('pedestrian');
+      consumeInformation('vehicle');
+    });
+  });
+};
+
+/**
+ * Consumes information with key="*.client.near"
+ */
+function consumeInformation(client) {
+
+  const key = `*.${client}.near`;
+
+  channel.assertQueue(
+    '',
+    {exclusive: true, autoDelete: true, durable: true },
+    (error2, q) => {
+      if (error2) {
+        console.log(`Could not assert queue to consume ${key}`);
+      }
+      channel.bindQueue(q.queue, exchange, key);
+      console.log(` Now consuming key (${key})`);
+      //consumes message
+      channel.consume(q.queue, function(msg) {
+         const message = JSON.parse(msg.content.toString());
+         console.log(" Message received '%s'", message);
+         addCrosswalkHistory(message);
+    }, {
+      noAck: false
     });
   });
 }
@@ -80,15 +93,12 @@ async function addCrosswalkHistory(message){
     client_id = message.ped_id;
   }
   const update = {$push: {history: { client: { id: client_id, kind: client }, crossedAt:Date.now()}}};
-  console.log("UPDATE");
-  console.log(update);
   try{
     const crosswalkHistory = await CrosswalksHistory.findOneAndUpdate({crosswalk_id: message.crosswalk_id}, update, {
       new: true,
       upsert: true
     });
-    console.log("ADICIONADO");
-    console.log(crosswalkHistory);
+    console.log("New information added to history!");
   }
   catch (error){
     console.log(error);
@@ -102,25 +112,18 @@ async function getInfo(id){
   let crosswalkHistory = [];
   let numPed = 0;
   let numVeh = 0;
-  const day = 86400000;
-  const today = new Date(Date.now());
-  const yesterday = new Date(Date.now() - day);
-  const filter = { "crosswalk_id": id, "history.crossedAt": {$gte: yesterday , $lt: today}};
+  const now = new Date();
+  const midnight = new Date(now.getFullYear(),now.getMonth(),now.getDate());
   try{
-  //  crosswalkHistory = await CrosswalksHistory.findOne(filter,"history.id history.kind");
     crosswalkHistory = await CrosswalksHistory.aggregate([
       {$match: {crosswalk_id: id}},
       {$project: {history:1}},
       {$unwind: '$history'},
-      {$match: {"history.crossedAt": {$gte: yesterday , $lt: today}}},
+      {$match: {"history.crossedAt": {$gte: midnight , $lt: now}}},
       {$group: {_id: { id: '$history.client.id', type: '$history.client.kind' }}},
     ]);
-    console.log("É ISTO QUE QUEREMOS");
-    console.log(crosswalkHistory);
-      crosswalkHistory.forEach(function(info){
-        console.log("Crosswalk Client");
-        console.log(info.client.kind);
-        if(info.client.kind == 'pedestrian'){
+      crosswalkHistory.forEach(function(entry){
+        if(entry._id.type == 'pedestrian'){
           numPed++;
         }
         else{
@@ -139,14 +142,14 @@ async function getInfo(id){
 /**
  * Simulator
  */
-
+/*
 function simulator(){
 
-  const info1={ped_id:11, crosswalk_id:'8e1d9202-8c15-4bc9-a07a-6ac16ca8d51b' };
-  const info3={ped_id:11, crosswalk_id:'8e1d9202-8c15-4bc9-a07a-6ac16ca8d51b' };
-  const info2={veh_id:44, crosswalk_id:'8e1d9202-8c15-4bc9-a07a-6ac16ca8d51b' };
-  const info4={veh_id:45, crosswalk_id:'8e1d9202-8c15-4bc9-a07a-6ac16ca8d51b' };
-  const info5={ped_id:6, crosswalk_id:'8e1d9202-8c15-4bc9-a07a-6ac16ca8d51b' };
+  const info1={veh_id:11, crosswalk_id:'eeebf21e-ceec-423d-899d-fe92564b7f79' };
+  const info3={veh_id:11, crosswalk_id:'eeebf21e-ceec-423d-899d-fe92564b7f79' };
+  const info2={ped_id:44, crosswalk_id:'68346ef5-2ab8-48ac-a90a-c69b13d627e3' };
+  const info4={ped_id:45, crosswalk_id:'68346ef5-2ab8-48ac-a90a-c69b13d627e3' };
+  const info5={veh_id:6, crosswalk_id:'68346ef5-2ab8-48ac-a90a-c69b13d627e3' };
 
 
   const exchange = 'private';
@@ -181,9 +184,10 @@ function simulator(){
 
 
 function seeInfo(){
-  console.log(getInfo('8e1d9202-8c15-4bc9-a07a-6ac16ca8d51b'));
+  console.log(getInfo('eeebf21e-ceec-423d-899d-fe92564b7f79'));
+  console.log(getInfo('68346ef5-2ab8-48ac-a90a-c69b13d627e3'));
 }
-
+*/
 
 
 app.get('/v1/crosswalks/:id', (req, res) => {
@@ -207,15 +211,14 @@ app.get('/v1/crosswalks/:id', (req, res) => {
 });
 
 
+
+
 const server = app.listen(3000, () => {
   const host = server.address().address;
   const port = server.address().port;
 
   console.log(`App listening at http://${host}:${port}`);
-  consumeInformation('pedestrian');
-  consumeInformation('vehicle');
-  setTimeout(simulator,5000);
-  setTimeout(seeInfo,8000);
+  startConnection();
 });
 
 module.exports = () => start();
